e8c056a39303d66172713d7ee11d836eaa933e15
rstott87 <rstott87@yahoo.com>
2023-02-06 23:40:24 -0500

    form inputs working

--- src/components/MortgageDisplay.jsx
+++ src/components/MortgageDisplay.jsx
@@ -1,29 +1,40 @@
+import { useState } from "react";
+
 function MortgageDisplay(props) {
+  const [totalHomePrice, setTotalHomePrice] = useState("100,000");
+  const [totalAmountBorrowed, setTotalAmountBorrowed] = useState("10,000");
+  const [interestRate, setInterestRate] = useState("3.7");
   return (
     <div className=" text-center border-4 rounded-lg shadow-lg text-2xlrounded-lg font-bold text-neutral-200 border-blue-800 w-1/3 p-8 bg-slate-700">
       {" "}
       <p className="text-center text-3xl mb-4">{props.title}</p>
       <form action="">
-        <label >
+        <label>
           <p>Total Home Price:</p>
           <input
             type="text"
-            className="border-2 border-blue-800 rounded-lg w-full p-2"
+            value={totalHomePrice}
+            onChange={(e) => setTotalHomePrice(e.target.value)}
+            className="text-slate-800 border-2 border-blue-800 rounded-lg w-full p-2"
           />
         </label>
         <label htmlFor="">
           <p className="text-center"> Amount Borrowed: </p>
-          <input
-            type="text"
-            className="border-2 border-blue-800 rounded-lg w-full p-2"
-          />
         </label>
+        <input
+          type="text"
+          value={totalAmountBorrowed}
+          onChange={(e) => setTotalAmountBorrowed(e.target.value)}
+          className="text-slate-800 border-2 border-blue-800 rounded-lg w-full p-2"
+        />
         <label htmlFor="">
           <p className="text-center"> Interest Rate: </p>
         </label>
         <input
           type="text"
-          className="border-2 border-blue-800 rounded-lg w-full p-2"
+          value={`${interestRate}`}
+          onChange={(e) => setInterestRate(e.target.value)}
+          className="text-slate-800 border-2 border-blue-800 rounded-lg w-full p-2"
         />
       </form>
     </div>
